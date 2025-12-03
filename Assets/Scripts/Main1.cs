ac7edbc294bb857e5982b30876e74cda5ed64a18
Lachesis <98927924+llKatzll@users.noreply.github.com>
2025-12-03 16:49:47 +0900

    야오오옥!!

--- Assets/Scripts/Main1.cs
+++ Assets/Scripts/Main1.cs
@@ -1,37 +1,71 @@
 using System.Threading;
 using System;
 using System.Diagnostics;
+using JetBrains.Annotations;
 
 namespace Program
 {
-    class Wizard
+    abstract class Animal
     {
-        // =======================================
-        // 1. Wizard 클래스를 만들어 다음 조건을 만족하세요:
-        // 
-        // - 멤버 변수: mp, intelligence (둘 다 int형)
-        // - 생성자에서 각각 50, -999으로 초기화
-        // - Main()에서 Wizard 객체를 생성하고 두 값을 출력
-        //
-        // [실행 결과]
-        // 마법사의 MP: 50, 지능: 20
-        // =======================================
+        public string Name;
 
-        int mp;
-        int intelligence;
-
-        public Wizard()
+        public Animal(string name)
         {
-            mp = 50;
-            intelligence = -999;
+            Name = name;
+            Console.WriteLine($"안녕 나는 개쩌는{Name}다");
         }
 
-        static void Main()
-        {
-            Wizard _wizard = new Wizard();
+        public abstract void MakeSound();
+    }
 
-            Console.WriteLine("마법사의 MP:" + _wizard.mp + ", 지능: " + _wizard.intelligence);
-            Console.WriteLine("듀..댜..뇌..창..던진..다!");
+    class Dog : Animal
+    {
+        public Dog(string name) : base(name) { } //Dog 만드는데 Animal 도 같이 갑시다
+
+        public override void MakeSound()
+        {
+            Console.Write("저는 가문의 명예를 이어받은 견(犬)의 귀공자,\n품위와 애정 모두 겸비한 강아지입니다.");
         }
     }
+
+    class Cat : Animal
+    {
+        public Cat(string name) : base(name) { }
+
+        public override void MakeSound()
+        {
+            for(int i = 0; i < 99; i++)
+            {
+                Console.WriteLine("야아아아오오오오오오오옥");
+            }
+        }
+    }
+
+    class Cow : Animal
+    {
+        public Cow(string name) : base(name) { }
+
+        public override void MakeSound()
+        {
+            Console.Write("저는 대지와 계절의 숨결을 품은,\n느긋함과 인내의 상징인 한 마리의 소입니다.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog("댕댕이");
+            dog.MakeSound();
+            Console.WriteLine();
+
+            Animal cat = new Cat("나비");
+            cat.MakeSound();
+            Console.WriteLine();
+
+            Animal cow = new Cow("해피카우");
+            cow.MakeSound();
+        }
+    }
+
 }
