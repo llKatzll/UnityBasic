a5e6c5bf3a424ca031968d2379f8e4d03901bfda
Lachesis <98927924+llKatzll@users.noreply.github.com>
2025-12-03 14:12:06 +0900

    ._.

--- Assets/Scripts/Main1.cs
+++ Assets/Scripts/Main1.cs
@@ -1,58 +1,155 @@
+using System.Threading;
 using System;
-using UnityEditor.PackageManager;
-using UnityEngine;
 
-public class Main1 : MonoBehaviour
+namespace Program
 {
-    //It took me ten years to find the answer to something, I forgot about it in 2 seconds, that's about it
-    static void Swap(int a, int b)
+    class Program
     {
-        int holder = a;
-        a = b;
-        b = holder;
-    }
-
-    static void Main()
-    {
-        int x = 10;
-        int y = 20;
-        Swap(x, y);
-        Debug.Log($"{x},{y}");
-
-        //지금 머만드는거징
-
-
-    }
-
-
-    //1+2+3+4+5+6 = 21 code? 지금 머 만드는거징
-    static int Sum(int start, int end)
-    {
-        if (start > end)
-            return 0;
-
-        return start + Sum(start + 1, end);
-        //머 만 드는 거 징 !? 재 귀 함 수!?
-    }
-
-    static int Sum2(int n, int o)
-    {
-        //이미 만든거 아닌감..
-        int sum = 0;
-
-        for (int i = n; i < o; i++)
+        enum ClassType
         {
-            sum += i;
+            None,
+            Knight,
+            Mage,
+            Rogue
         }
 
-        return sum;
-    }
+        enum MonsterType
+        {
+            None,
+            Slime,
+            Orc,
+            Skeleton
+        }
 
-    static void Add(int a, int b, int c = 0, int d = 0, int e = 1)
-    {
-        //흠..
-        int result = a + b + c + d + e;
-        Console.WriteLine($"합계: {result}");
+
+        struct Player
+        {
+            public int hp;
+            public int atk;
+        }
+
+        struct Monster
+        {
+            public int hp;
+            public int atk;
+        }
+
+        static ClassType ClassChoice()
+        {
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 마법사");
+            Console.WriteLine("[3] 도둑");
+
+            ClassType choice = ClassType.None;
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.Knight;
+                    break;
+                case "2":
+                    choice = ClassType.Mage;
+                    break;
+                case "3":
+                    choice = ClassType.Rogue;
+                    break;
+            }
+
+            return choice;
+        }
+
+        static void CreatePlayer(ClassType choice, out Player player)
+        {
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.atk = 10;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.atk = 15;
+                    break;
+                case ClassType.Rogue:
+                    player.hp = 75;
+                    player.atk = 12;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.atk = 0;
+                    break;
+            }
+        }
+
+        static void CreateRandomMonster(out Monster monster)
+        {
+            var _rand = new Random();
+
+            int _value = _rand.Next(0, 100);
+
+            MonsterType _type;
+
+            if(_value < 20)  //0~19 20%
+            {
+                _type = MonsterType.Slime;
+            }
+            else if (_value < 60) //20~59 40%
+            {
+                _type = MonsterType.Orc;
+            }
+            else if(_value < 90) //60~89 30%
+            {
+                _type = MonsterType.Skeleton;
+            }
+            else //10% GETOUT
+            {
+                _type = MonsterType.None;
+            }
+
+            monster = new Monster();
+
+            switch (_type)
+            {
+                case MonsterType.Slime:
+                    monster.hp = 15;
+                    monster.atk = 2;
+                    break;
+                case MonsterType.Orc:
+                    monster.hp = 50;
+                    monster.atk = 5;
+                    break;
+                case MonsterType.Skeleton:
+                    monster.hp = 25;
+                    monster.atk = 8;
+                    break;
+                default:
+                    monster.hp = 0;
+                    monster.atk = 0;
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ClassType choice = ClassType.None;
+
+            Player player;
+
+            while (true)
+            {
+                choice = ClassChoice();
+                if (choice != ClassType.None)
+                {
+                    CreatePlayer(choice, out player);
+
+                    Console.WriteLine($"HP {player.hp}, ATK {player.atk}");
+
+                    Monster monster;
+                    CreateRandomMonster(out monster);
+                }
+            }
+        }
     }
 }
-
